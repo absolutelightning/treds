5e439e2524b16b1eed1718711af228322f0649e4
absolutelightning <ashesh.vidyut@hashicorp.com>
2024-09-10 18:25:34 +0530

    add mget and prefix scan keys

---
+++ commands/mget.go
@@ -0,0 +1,77 @@
+package commands
+
+import (
+	"bytes"
+	"fmt"
+	"golang.org/x/sync/errgroup"
+	"sync"
+	"treds/store"
+)
+
+const MGetCommand = "MGET"
+
+func RegisterMGetCommand(r CommandRegistry) {
+	r.Add(&CommandRegistration{
+		Name:     MGetCommand,
+		Validate: validateMGet(),
+		Execute:  executeMGet(),
+	})
+}
+
+func validateMGet() ValidationHook {
+	return func(args []string) error {
+		if len(args) == 0 {
+			return fmt.Errorf("expected atlest 1 argument, got %d", len(args))
+		}
+
+		return nil
+	}
+}
+
+func executeMGet() ExecutionHook {
+	return func(args []string, store store.Store) (string, error) {
+		results := make([]string, len(args))
+
+		// Create an error group
+		var g errgroup.Group
+
+		// Use a mutex to protect writing to the results slice
+		var mu sync.Mutex
+
+		// Loop through each key to retrieve
+		for i, arg := range args {
+			// Capture the current index and key to avoid closure issues
+			index := i
+			key := arg
+
+			// Start a new goroutine for each key
+			g.Go(func() error {
+				// Get the result from the store
+				res, err := store.Get(key)
+				if err != nil {
+					return err // Return error to errgroup
+				}
+
+				// Use the mutex to protect access to the results slice
+				mu.Lock()
+				results[index] = res
+				mu.Unlock()
+
+				return nil
+			})
+		}
+
+		// Wait for all goroutines to finish and check for any errors
+		if err := g.Wait(); err != nil {
+			return "", err
+		}
+
+		// Concatenate results to maintain order
+		var response bytes.Buffer
+		for _, res := range results {
+			response.WriteString(fmt.Sprintf("%v\n", res))
+		}
+
+		return fmt.Sprintf("%v\n", response.String()), nil
+	}
+}
--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.22
 require (
 	github.com/absolutelightning/radix v1.0.6
 	github.com/tidwall/evio v1.0.8
+	golang.org/x/sync v0.8.0
 )
 
 require github.com/kavu/go_reuseport v1.5.0 // indirect
